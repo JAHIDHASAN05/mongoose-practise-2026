72f2d6702296d2bd8623c7fcb2ba56a78a57aa22
JAHID HASAN <developerjahid05@gmail.com>
2026-02-01 16:06:37 +0600

    project setup without prettier

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import app from "./app";
+import config from "./config";
+
+import mongoose from "mongoose";
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://beckend_level_2_2026:AEtRrsTiWYPWJcmv@cluster0.qm82exp.mongodb.net/?appName=Cluster0",
+    );
+
+    // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
