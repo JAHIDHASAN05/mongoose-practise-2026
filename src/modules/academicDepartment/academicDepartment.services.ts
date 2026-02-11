3e2a5879128489845bce6dabb1bfec0b2bf8c2d2
JAHID HASAN <developerjahid05@gmail.com>
2026-02-11 17:39:12 +0600

    feat: get all academic poplulate academic faculty data

--- src/modules/academicDepartment/academicDepartment.services.ts
+++ src/modules/academicDepartment/academicDepartment.services.ts
@@ -1,21 +1,25 @@
 import { TAcademicDepartment } from "./academicDepartment.interface";
 import { academicDepartment } from "./academicDepartment.model";
 
-const createAcademicDepartmeentIntoDB=async(payload:TAcademicDepartment)=>{
-    const isDeparmentExist= await academicDepartment.findOne({name:payload.name})
-    if(isDeparmentExist){
-        throw new Error(`${payload.name} is already exist`)
-    }
-    const result= await academicDepartment.create(payload)
-    return result
-}
+const createAcademicDepartmeentIntoDB = async (
+  payload: TAcademicDepartment,
+) => {
+  const isDeparmentExist = await academicDepartment.findOne({
+    name: payload.name,
+  });
+  if (isDeparmentExist) {
+    throw new Error(`${payload.name} is already exist`);
+  }
+  const result = await academicDepartment.create(payload);
+  return result;
+};
 
-const getAllAcademicDepartmentFromDB= async()=>{
-console.log("object");
-     const result = await academicDepartment.find()
-     return result
-}
-export const academicDepartmentServices={
-    createAcademicDepartmeentIntoDB,
-    getAllAcademicDepartmentFromDB
-}
+const getAllAcademicDepartmentFromDB = async () => {
+  console.log("object");
+  const result = await academicDepartment.find().populate("academicFaculty");
+  return result;
+};
+export const academicDepartmentServices = {
+  createAcademicDepartmeentIntoDB,
+  getAllAcademicDepartmentFromDB,
+};
