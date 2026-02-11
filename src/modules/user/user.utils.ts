69025b16f20d909dae022b13ba50baeeaa97a5e1
JAHID HASAN <developerjahid05@gmail.com>
2026-02-11 15:06:19 +0600

    refector: user custom uniqe id bug fix

--- src/modules/user/user.utils.ts
+++ src/modules/user/user.utils.ts
@@ -22,21 +22,11 @@ import { userModel } from "./user.model";
   })
   .lean();
 
-  return lastStudent?.id ? lastStudent.id : undefined;
+  return lastStudent?.id ? lastStudent.id.substring(6) : undefined;
 };
 export const generateStudentId = async (payload: TAcademicSemister) => {
-  let currentId =  '0';
- const lastStudentId= await findLastStudentId(payload.year, payload.code) 
- const lastStudentSemisterCode= lastStudentId?.substring(4,6)
- const lastStudentSemisteryear=lastStudentId?.substring(0,4)
- const currentStudentSemisterCode=payload?.code
- const currentStudentSemisterYear=payload?.year
+  let currentId = await findLastStudentId(payload.year, payload.code)|| '0';
 
-
- if(lastStudentId && lastStudentSemisterCode ===currentStudentSemisterCode && lastStudentSemisteryear===currentStudentSemisterYear){
-    currentId=lastStudentId.substring(6)
- }
-  console.log({currentId});
   let incrementId = (Number(currentId || 0) + 1).toString().padStart(4, "0");
   incrementId=`${payload?.year}${payload?.code}${incrementId}`
   console.log({payload});
