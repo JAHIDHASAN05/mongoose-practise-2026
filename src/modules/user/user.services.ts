c3d1fd508227341d4143658edc77445ebfea43e7
JAHID HASAN <developerjahid05@gmail.com>
2026-02-12 15:02:04 +0600

    feat: transation and roll back for creating user

--- src/modules/user/user.services.ts
+++ src/modules/user/user.services.ts
@@ -1,12 +1,13 @@
-import { NextFunction } from "express";
 import { Student } from "../students/students.interface";
 import { studentModel } from "../students/students.model";
 import { TUser } from "./user.interface";
 import { userModel } from "./user.model";
 import { generateStudentId } from "./user.utils";
 import { academicSemisterModel } from "../academicSemister/academicSemister.model";
-import { Types } from "mongoose";
+import mongoose, { Types } from "mongoose";
 import AppError from "../../error/AppError";
+import { academicDepartment } from "../academicDepartment/academicDepartment.model";
+
 
 const CreateUserIntoDB = async (password: string, student: Student) => {
   let user: Partial<TUser> = {
@@ -15,31 +16,59 @@ const CreateUserIntoDB = async (password: string, student: Student) => {
     role: "student",
   };
 
-  const admissionSemister = await academicSemisterModel.findOne({
+  const session = await mongoose.startSession()
+
+  try {
+    session.startTransaction()
+  
+     const admissionSemister = await academicSemisterModel.findOne({
     _id: new Types.ObjectId(student.admissionSemister),
-  });
+  }).session(session);
   if (!admissionSemister) {
+
+
     throw new AppError(400, "admistion semister is not valid");
   }
-  const academicDepartment = await academicSemisterModel.findOne({
+  const academicDepartmentExist = await academicDepartment.findOne({
+    
     _id: new Types.ObjectId(student.academicDepartment),
-  });
+  }).session(session);
 
-  if (!academicDepartment) {
+  if (!academicDepartmentExist) {
+
+    
+    console.log({academicDepartment},student.academicDepartment);
     throw new AppError(400, "academic Department  is not valid");
   }
 
   user.id = await generateStudentId(admissionSemister);
 
-  const result = await userModel.create(user);
+  const result = await userModel.create([user],{session});
 
-  if (result._id) {
-    ((student.id = result.id), (student.userId = result._id));
-
-    const newStudent = await studentModel.create(student);
-
-    return newStudent;
+  if (!result.length) {
+    throw new AppError(404,'Failed to create user')
   }
+      ((student.id = result[0].id), (student.userId = result[0]._id));
+
+    const newStudent = await studentModel.create([student],{session});
+
+
+     if(!newStudent.length){
+        throw new AppError(404,'Failed to create student')
+     }
+
+     await session.commitTransaction()
+     await session.endSession()
+    return newStudent;
+  } catch (error) {
+
+    await session.abortTransaction()
+    await session.endSession()
+    throw error
+  }
+
+
+ 
 };
 
 export const UserServices = {
